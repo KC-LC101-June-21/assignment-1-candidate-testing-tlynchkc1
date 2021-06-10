cdb05df8d386c5d246badd9243c2c29ce32771ec
tlynchkc <tlynchkc@gmail.com>
2021-06-10 07:41:25 +0000

    Removed comments in front of question, and correctAnswer variables

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-// let question = "Who was the first American woman in space? ";
-// let correctAnswer = "Sally Ride";
-// let candidateAnswer;
+ let question = "Who was the first American woman in space? ";
+ let correctAnswer = "Sally Ride";
+ let candidateAnswer;
 
 
 // console.log(`Part 1`);
